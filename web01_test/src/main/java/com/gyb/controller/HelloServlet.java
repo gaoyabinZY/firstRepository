c5e2b66224c542e65fcc4cd69ff6b2f366a6047c
gyb <gaoyabinZY@163.com>
2023-12-23 19:28:42 +0800

    sdflaskdjflaksdjfl

---
+++ web01_test/src/main/java/com/gyb/controller/HelloServlet.java
@@ -0,0 +1,24 @@
+package com.gyb.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=utf-8");
+        PrintWriter pw = resp.getWriter();
+        pw.println("helloMaven");
+        pw.flush();
+        pw.close();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    }
+}
---
+++ web01_test/src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+<a href="hello">访问HelloServlet</a>
+</body>
+</html>
